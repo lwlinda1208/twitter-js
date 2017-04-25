eb2548a075da3470dcb071a0957fbb63284a9cce
Allie Fauer <alliefauer@gmail.com>
2017-04-25 11:27:44 -0400

    edits allies computer

--- app.js
+++ app.js
@@ -1,15 +1,42 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
 const app = express();
+const tweetBank = require('./tweetBank');
+const routes = require('./routes');
+
+
 
 app.use('/', function(req, res, next){
 	console.log(req.method + " " + req.originalUrl);
 	next();
 });
 
-app.get('/news', function(req, res, next){
-	res.send("Here is the news!!!");
-});
+app.use('/', routes);
 
 var server = app.listen("3000", function(){
 	console.log("listening");
 });
+
+
+var locals = {
+	title : 'An Example',
+	people : [ {name: 'Allie'},
+			{name: 'Jackie'}]
+}
+
+const people = [{name: 'Allie'}, {name: 'Jackie'}];
+
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+//res.render('index', {title: 'Pair Programming Budz', people: people});
+
+// app.get('/views/index.html', function(req, res, next) {
+// 	// nunjucks.configure('views', {noCache: true});
+// 	// nunjucks.render('index.html', locals, function(err, output) {
+// 	// 	res.send(output);
+// 	// })
+	
+// });
+
+// console.log(tweetBank.add('Jackie', 'I know so much about nunjucks and nothing at all!'));
+// console.log(tweetBank.find({name: 'Jackie'}));
